7a41a527277b1613418057a9dddb51cf0a3da50b
s142162-RU <s142162@student.squ.edu.om>
2026-05-03 01:46:11 +0400

    Implement delete note functionality in API

--- api/delete_note.php
+++ api/delete_note.php
@@ -1,1 +1,14 @@
+<?php
+header('Content-Type: application/json');
+$data = json_decode(file_get_contents('php://input'), true);
+$system = $data['system'] ?? '';
+$organ_id = $data['organ_id'] ?? '';
 
+$filename = "notes_{$system}.json";
+if (file_exists($filename)) {
+    $allNotes = json_decode(file_get_contents($filename), true);
+    unset($allNotes[$organ_id]);
+    file_put_contents($filename, json_encode($allNotes));
+}
+echo json_encode(['success' => true]);
+?>
